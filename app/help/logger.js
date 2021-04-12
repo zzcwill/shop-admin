90c7986eb04ec8cdb829975793f663880ac9cc47
zhengzhichao <377950622@qq.com>
2021-04-12 23:12:38 +0800

    log4js优化

--- app/help/logger.js
+++ app/help/logger.js
@@ -3,11 +3,11 @@ var log4js = require('log4js');
 var config = global.config
 
 log4js.configure({
-  appenders: { cheese: { type: 'file', filename: path.join(config.log_dir, 'cheese.log') } },
+  appenders: { cheese: { type: 'file', filename: path.resolve(config.log_dir, 'cheese.log') } },
   categories: { default: { appenders: ['cheese'], level: 'error' } }
 });
 
 var logger = log4js.getLogger('cheese');
-logger.level = process.env.NODE_ENV !== 'dev' ? 'DEBUG' : 'ERROR'
+logger.level = process.env.NODE_ENV !== 'dev' ? 'DEBUG' : 'ERROR';
 
 module.exports = logger
